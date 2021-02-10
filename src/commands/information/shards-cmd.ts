318dfbab106ad8c74ef2a58f86bc50fd185ddaf2
Donovan Daniels <dwddaniels262@gmail.com>
2021-02-10 02:57:43 -0600

    sick bug

--- src/commands/information/shards-cmd.ts
+++ src/commands/information/shards-cmd.ts
@@ -21,7 +21,7 @@ export default new Command(["shards"], __filename)
 					value: [
 						`{lang:other.words.status$ucwords$}: ${Strings.ucwords(s.status)}`,
 						`{lang:other.words.latency$ucwords$}: ${config.emojis.default.circle[s.latency <= 100 ? "green" : s.latency <= 300 ? "yellow" : "red"]} **${s.latency}ms**`,
-						`{lang:other.words.servers$ucwords$}: ${this.guilds.filter(g => g.shard.id === s.id)}`
+						`{lang:other.words.servers$ucwords$}: ${this.guilds.filter(g => g.shard.id === s.id).length}`
 					].join("\n"),
 					inline: true
 				})))
